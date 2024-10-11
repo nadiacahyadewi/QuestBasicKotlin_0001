a1e5681cc0a9172607fcd49a486f52f9149b3779
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-10-12 00:45:41 +0700

    Menambahkan fungsi nullsafety dengan contoh variabel nullable dan non-nullable, serta cara mengatasi nilai null dalam kondisi

--- app/src/main/java/com/example/praktikum1/NullSafety.kt
+++ app/src/main/java/com/example/praktikum1/NullSafety.kt
@@ -1,6 +1,19 @@
 package com.example.praktikum1
 
 fun nullsafety() {
-// neverNull has String type
+    // neverNull has String type
     var neverNull: String = "This can't be null"
+    // Throws a compiler error
+    // neverNull = null
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null her e"
+    // This is OK
+    nullable = null
+    // Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
 }
