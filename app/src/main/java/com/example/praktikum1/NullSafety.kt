9e58034829263389d45a6c84eeb4fa887e1ed17a
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-10-12 00:47:36 +0700

    Menambahkan fungsi nullsafety dengan safe call operator dan contoh fungsi yang hanya menerima variabel non-nullable

--- app/src/main/java/com/example/praktikum1/NullSafety.kt
+++ app/src/main/java/com/example/praktikum1/NullSafety.kt
@@ -15,5 +15,13 @@ fun nullsafety() {
     } else {
         println("inferredNonNull is not null")
     }
-
+    // Safe call operator
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+    // notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+    println(strLength (neverNull)) // 18
 }
+
