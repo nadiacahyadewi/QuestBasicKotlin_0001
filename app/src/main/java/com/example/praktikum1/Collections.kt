89b0cce706f310591dbf6a5682e799abb49d155b
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-10-11 23:49:23 +0700

    Menambahkan fungsi ContohList yang menunjukkan penggunaan list read-only dengan menampilkan abjad A, B, dan C.

---
+++ app/src/main/java/com/example/praktikum1/Collections.kt
@@ -0,0 +1,14 @@
+package com.example.praktikum1
+
+// list adalah kumpulan data yang disusun secara berurutan dari
+// list bersifat ordered, artinya data yang dimasukan akan memiliki posisi sesuai urutan
+// list bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// list read-only menggunakan listOf
+// list mutable menggunakan mutableListOf
+
+fun ContohList() {
+    println("=== List ===")
+    //list read-only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+}
