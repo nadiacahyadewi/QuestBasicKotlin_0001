0b1ff7177c677dbc64090984d3744a5037cd518e
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-10-11 23:30:11 +0700

    menambahkan fungsi uppercaseString untuk mengubah string menjadi huruf kapital

--- app/src/main/java/com/example/praktikum1/LambdaFunction.kt
+++ app/src/main/java/com/example/praktikum1/LambdaFunction.kt
@@ -1,2 +1,10 @@
 package com.example.praktikum1
 
+// lambda functions
+// lambda functions adalah fungsi yang tidak memiliki nama
+// lambda functions biasanya digunakan untuk membuat fungsi yang sederhana
+// lambda functions menggunakan tanda panah (->)  untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
