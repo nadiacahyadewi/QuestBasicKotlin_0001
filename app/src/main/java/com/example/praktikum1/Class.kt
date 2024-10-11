e0a4d4ecb00a97a1b318a931f361ba19f976ca42
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-10-11 21:37:32 +0700

    membuat fungsi utama pada contact yang berisi id dan email serta menampilkan isi email dari class contact

--- app/src/main/java/com/example/praktikum1/Class.kt
+++ app/src/main/java/com/example/praktikum1/Class.kt
@@ -3,3 +3,11 @@ package com.example.praktikum1
 class Motor
 
 class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1,"mary@gmail.com")
+
+    // prints the value of the property: email
+    println(contact.email)
+
+}
