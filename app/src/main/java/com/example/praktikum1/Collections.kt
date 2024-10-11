552b4a00b6857da111afcd342979f398689bbd2c
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-10-12 00:23:46 +0700

    menambah rectangle kedalam set mutable dan menghapus circle dari set mutable

--- app/src/main/java/com/example/praktikum1/Collections.kt
+++ app/src/main/java/com/example/praktikum1/Collections.kt
@@ -51,5 +51,12 @@ fun ContohSet() {
     val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
     println(shape)
 
+// Menambahkan data ke dalam Set Mutable
+    shape.add("Rectangle")
+    println(shape)
+
+    // Menghapus data dari Set Mutable
+    shape.remove("Circle")
+    println(shape)
 
 }
