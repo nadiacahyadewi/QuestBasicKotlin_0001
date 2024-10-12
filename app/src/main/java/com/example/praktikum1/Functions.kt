cf16871b2dc624cd5cf7ccfa638bb4ddf25d7d49
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-10-12 13:41:00 +0700

    Menambahkan fungsi withoutParameter untuk menampilkan pesan tanpa parameter dan fungsi withParameter untuk menampilkan pesan dengan nama

---
+++ app/src/main/java/com/example/praktikum1/Functions.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum1
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
