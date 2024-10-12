741d9616f2c53d2e0d96027663ec437bf4bbd663
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-10-12 14:07:33 +0700

    membuat tipe data

---
+++ app/src/main/java/com/example/praktikum1/TipeData.kt
@@ -0,0 +1,44 @@
+package com.example.praktikum1
+
+fun main() {
+    //Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //Short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //Long
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    //Float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    //Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    //Char
+    val charValue: Char = 'A'
+    println(charValue)
+    // Bisa langsung panggil kalo tidak di dalam string yg petik dua " "
+
+    //Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //String
+    var stringValue: String = "Hello, World!"
+    println("String $stringValue")
+
+    stringValue = "Hello, Nadia"
+    println("String $stringValue")
+
+}
